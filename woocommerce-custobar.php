6e16d6c8f228d5f3251151cb6249e36488197c06
Casey Milne <joel@goldhat.ca>
2020-03-31 19:12:37 -0500

    edit namespaces

--- woocommerce-custobar.php
+++ woocommerce-custobar.php
@@ -13,13 +13,13 @@ define( 'WOOCOMMERCE_CUSTOBAR_PATH', plugin_dir_path( __FILE__ ) );
 define( 'WOOCOMMERCE_CUSTOBAR_URL', plugin_dir_url( __FILE__ ) );
 define( 'WOOCOMMERCE_CUSTOBAR_VERSION', '1.0.0' );
 
-require_once(__DIR__ . '/vendor/autoload.php');
-require_once(__DIR__ . '/config.php');
-require_once(__DIR__ . '/includes/functions.php');
+require_once(WOOCOMMERCE_CUSTOBAR_PATH . '/vendor/autoload.php');
+require_once(WOOCOMMERCE_CUSTOBAR_PATH . '/config.php');
+require_once(WOOCOMMERCE_CUSTOBAR_PATH . '/includes/functions.php');
 
-\Sofokus\WooCommerceCustobar\load_localizations();
+\WooCommerceCustobar\load_localizations();
 
-register_activation_hook(__FILE__, [\Sofokus\WooCommerceCustobar\Plugin::class, 'activate']);
-register_deactivation_hook(__FILE__, [\Sofokus\WooCommerceCustobar\Plugin::class, 'deactivate']);
+register_activation_hook(__FILE__, [\WooCommerceCustobar\Plugin::class, 'activate']);
+register_deactivation_hook(__FILE__, [\WooCommerceCustobar\Plugin::class, 'deactivate']);
 
-\Sofokus\WooCommerceCustobar\plugin()->initialize();
+\WooCommerceCustobar\plugin()->initialize();
