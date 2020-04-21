7cfbab7e7b1f08092663cc2288cc71af57e373f5
Casey Milne <joel@goldhat.ca>
2020-04-20 22:37:31 -0500

    sync section styling

--- templates/sync-report.php
+++ templates/sync-report.php
@@ -1,15 +1,15 @@
 <div id="custobar-export-wrap">
 
-  <h2>Custobar Export Sync Status</h2>
-  <button id="custobar-export">Run Exporter</button>
+  <h2>Custobar Sync Status</h2>
 
   <table>
     <tr>
       <th>Record Type</th>
-      <th>Total Count</th>
-      <th>Synced Count</th>
+      <th>Total Records</th>
+      <th>Synced Records</th>
       <th>Sync %</th>
       <th>Last Export</th>
+      <th>&nbsp;</th>
     </tr>
     <tr>
       <td>Products</td>
@@ -17,6 +17,7 @@
       <td><?php print $productStat->synced; ?></td>
       <td><?php print ($productStat->synced / $productStat->total) * 100 . '%'; ?></td>
       <td>2020-04-10 02:34AM</td>
+      <td><button id="custobar-export">Run Exporter</button></td>
     </tr>
     <tr>
       <td>Sales</td>
@@ -24,6 +25,7 @@
       <td><?php print $saleStat->synced; ?></td>
       <td><?php print ($saleStat->synced / $saleStat->total) * 100 . '%'; ?></td>
       <td>2020-04-10 02:34AM</td>
+      <td><button id="custobar-export">Run Exporter</button></td>
     </tr>
     <tr>
       <td>Customers</td>
@@ -31,6 +33,7 @@
       <td><?php print $customerStat->synced; ?></td>
       <td><?php print ($customerStat->synced / $customerStat->total) * 100 . '%'; ?></td>
       <td><?php print $customerStat->updated; ?></td>
+      <td><button id="custobar-export">Run Exporter</button></td>
     </tr>
   </table>
 
