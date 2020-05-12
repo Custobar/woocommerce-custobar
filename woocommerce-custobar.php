c220bb4a89e7e926dc331e832eb34d6c3b47577d
Casey Milne <joel@goldhat.ca>
2020-05-12 10:09:55 -0500

    release 1.3.0

--- woocommerce-custobar.php
+++ woocommerce-custobar.php
@@ -5,14 +5,14 @@
  * Description: Syncs your WooCommerce data with Custobar CRM.
  * Author: Custobar CRM
  * Text Domain: woocommerce-custobar
- * Version: 1.2.0
+ * Version: 1.3.0
  * Domain Path: /languages
  * WC requires at least: 3.0
  */
 
 define( 'WOOCOMMERCE_CUSTOBAR_PATH', plugin_dir_path( __FILE__ ) );
 define( 'WOOCOMMERCE_CUSTOBAR_URL', plugin_dir_url( __FILE__ ) );
-define( 'WOOCOMMERCE_CUSTOBAR_VERSION', '1.2.0' );
+define( 'WOOCOMMERCE_CUSTOBAR_VERSION', '1.3.0' );
 
 require_once(WOOCOMMERCE_CUSTOBAR_PATH . '/includes/loader.php');
 
