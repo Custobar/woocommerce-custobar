cf44fcadc681cc09ad3b0f9b7f4a8174da570155
Lari Marsalo <lari@contrast.fi>
2020-12-07 15:31:06 +0200

    Add phpcs and WordPress sniffs
    This helps to achieve WP/WC coding standards

--- woocommerce-custobar.php
+++ woocommerce-custobar.php
@@ -1,25 +1,28 @@
 <?php
 
 /**
- * Plugin name: WooCommerce Custobar
+ * Plugin name: Custobar for WooCommerce (dev)
  * Description: Syncs your WooCommerce data with Custobar.
  * Author: Custobar
  * Text Domain: woocommerce-custobar
- * Version: 1.3.9
+ * Version: 1.4.1
  * Domain Path: /languages
  * WC requires at least: 4.0
  * Requires PHP 7.2+
  */
+
 defined('ABSPATH') or exit;
 
 if (!defined('WOOCOMMERCE_CUSTOBAR_PATH')) {
-    define( 'WOOCOMMERCE_CUSTOBAR_PATH', plugin_dir_path( __FILE__ ) );
+    define('WOOCOMMERCE_CUSTOBAR_PATH', plugin_dir_path(__FILE__));
 }
+
 if (!defined('WOOCOMMERCE_CUSTOBAR_URL')) {
-    define( 'WOOCOMMERCE_CUSTOBAR_URL', plugin_dir_url( __FILE__ ) );
+    define('WOOCOMMERCE_CUSTOBAR_URL', plugin_dir_url(__FILE__));
 }
+
 if (!defined('WOOCOMMERCE_CUSTOBAR_VERSION')) {
-    define( 'WOOCOMMERCE_CUSTOBAR_VERSION', '1.4.0' );
+    define('WOOCOMMERCE_CUSTOBAR_VERSION', '1.4.1');
 }
 
 require_once(WOOCOMMERCE_CUSTOBAR_PATH . '/includes/loader.php');
