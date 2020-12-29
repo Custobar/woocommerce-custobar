a1c76a8a7f7c314e1ca4fc63fa090c37af9fd223
Lari Marsalo <lari@contrast.fi>
2020-12-29 11:35:30 +0200

    Remove unused code
    * fixes #7

--- includes/functions.php
+++ includes/functions.php
@@ -14,7 +14,7 @@ defined( 'ABSPATH' ) or exit;
  * @return Plugin
  */
 function plugin() {
-	 global $plugin;
+	global $plugin;
 
 	if ( ! $plugin instanceof Plugin ) {
 		$plugin = new Plugin();
@@ -47,26 +47,4 @@ function load_localizations() {
 			load_textdomain( $domain, get_plugin_dir() . '/languages/' . $mofile );
 		}
 	);
-}
-
-/**
- * Loads given locale file if found.
- * Plugin's default language is in English therefore no language file, only unload
- *
- * @param null|string $locale 'en' to unload to plugin own language, otherwise locale code
- */
-function load_specific_plugin_textdomain( $locale = null ) {
-	$domain = 'woocommerce-custobar';
-	if ( $locale === null ) {
-		$locale = apply_filters( 'plugin_locale', is_admin() ? get_user_locale() : get_locale(), $domain );
-	}
-	if ( $locale === 'en' ) {
-		unload_textdomain( $domain );
-		return;
-	}
-	$mofile = $domain . '-' . $locale . '.mo';
-	if ( file_exists( get_plugin_dir() . '/languages/' . $mofile ) ) {
-		unload_textdomain( $domain );
-		load_textdomain( $domain, get_plugin_dir() . '/languages/' . $mofile );
-	}
 }
