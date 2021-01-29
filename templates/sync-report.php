5b9d7eeb74821b64b26db8403536e270dab0aa45
Lari Marsalo <lari@contrast.fi>
2021-01-29 15:53:38 +0200

    Escape output
    * Fixes #11
    
    Please enter the commit message for your changes. Lines starting

--- templates/sync-report.php
+++ templates/sync-report.php
@@ -15,34 +15,32 @@
 	<tbody>
 		<tr class="sync-report-product">
 			<td>Products</td>
-			<td class="custobar-center"><?php echo $product_stat->total; ?> / <?php echo $product_stat->variant_total; ?></td>
-			<td class="custobar-center"><?php echo $product_stat->synced; ?> / <?php echo $product_stat->variant_synced; ?></td>
-			<td class="custobar-center"><?php echo $product_stat->synced_percent; ?></td>
-			<td><?php echo $product_stat->last_updated; ?></td>
+			<td class="custobar-center"><?php echo esc_html($product_stat->total); ?> / <?php echo esc_html($product_stat->variant_total); ?></td>
+			<td class="custobar-center"><?php echo esc_html($product_stat->synced); ?> / <?php echo esc_html($product_stat->variant_synced); ?></td>
+			<td class="custobar-center"><?php echo esc_html($product_stat->synced_percent); ?></td>
+			<td><?php echo esc_html($product_stat->last_updated); ?></td>
 			<td><input name="reset-product" type="checkbox" value="1"></td>
 			<td><button class="custobar-export" data-record-type="product">Run Exporter</button></td>
 		</tr>
 		<tr class="sync-report-customer">
 			<td>Customers</td>
-			<td class="custobar-center"><?php echo $customer_stat->total; ?></td>
-			<td class="custobar-center"><?php echo $customer_stat->synced; ?></td>
-			<td class="custobar-center"><?php echo $customer_stat->synced_percent; ?></td>
-			<td><?php echo $customer_stat->last_updated; ?></td>
+			<td class="custobar-center"><?php echo esc_html($customer_stat->total); ?></td>
+			<td class="custobar-center"><?php echo esc_html($customer_stat->synced); ?></td>
+			<td class="custobar-center"><?php echo esc_html($customer_stat->synced_percent); ?></td>
+			<td><?php echo esc_html($customer_stat->last_updated); ?></td>
 			<td><input name="reset-customer" type="checkbox" value="1"></td>
 			<td><button class="custobar-export" data-record-type="customer">Run Exporter</button></td>
 		</tr>
 		<tr class="sync-report-sale">
 			<td>Sales</td>
-			<td class="custobar-center"><?php echo $sale_stat->total; ?></td>
-			<td class="custobar-center"><?php echo $sale_stat->synced; ?></td>
-			<td class="custobar-center"><?php echo $sale_stat->synced_percent; ?></td>
-			<td><?php echo $sale_stat->last_updated; ?></td>
+			<td class="custobar-center"><?php echo esc_html($sale_stat->total); ?></td>
+			<td class="custobar-center"><?php echo esc_html($sale_stat->synced); ?></td>
+			<td class="custobar-center"><?php echo esc_html($sale_stat->synced_percent); ?></td>
+			<td><?php echo esc_html($sale_stat->last_updated); ?></td>
 			<td><input name="reset-sale" type="checkbox" value="1"></td>
 			<td><button class="custobar-export" data-record-type="sale">Run Exporter</button></td>
 		</tr>
 	<tbody>
 	</table>
-
 </div>
-
 <hr />
