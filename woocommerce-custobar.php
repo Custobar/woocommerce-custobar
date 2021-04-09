7b91058a261493b877834b1a4557b16d4c83719b
Sami Mäkelä <sami@contrast.fi>
2021-04-09 06:57:16 +0300

    Bump version

--- woocommerce-custobar.php
+++ woocommerce-custobar.php
@@ -5,7 +5,7 @@
  * Description: Syncs your WooCommerce data with Custobar.
  * Author: Custobar
  * Text Domain: woocommerce-custobar
- * Version: 2.2.0
+ * Version: 2.3.0
  * Domain Path: /languages
  * WC requires at least: 4.0
  * Requires PHP 7.2+
