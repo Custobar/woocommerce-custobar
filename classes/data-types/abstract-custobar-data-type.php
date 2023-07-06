0cbfd187968967be314de26c7bfee5d430c452ca
Samuel Gemmell <samuel@mysteerio.fi>
2023-07-06 13:16:30 +0300

    Add support for Custobar additional fields

--- classes/data-types/abstract-custobar-data-type.php
+++ classes/data-types/abstract-custobar-data-type.php
@@ -2,7 +2,7 @@
 
 namespace WooCommerceCustobar\DataType;
 
-defined( 'ABSPATH' ) || exit;
+defined('ABSPATH') || exit;
 
 /**
  * Class Custobar_Data_Type
@@ -11,55 +11,60 @@ defined( 'ABSPATH' ) || exit;
  *
  * @package WooCommerceCustobar\DataType
  */
-abstract class Custobar_Data_Type {
+abstract class Custobar_Data_Type
+{
 
 
 	protected static $default_keys = array();
 
-	public function __construct() {
-		static::$default_keys = static::get_default_keys();
+	public function __construct()
+	{
+		static::$default_keys = array_keys(static::get_fields_map());
 	}
 
-	public function get_assigned_properties() {
+	public function get_assigned_properties()
+	{
 		$data_source_fields = $this->data_source->get_fields();
-		$fields_map         = static::get_fields_map();
-		$properties         = array();
+		$fields_map = static::get_fields_map();
+		$properties = array();
 
-		foreach ( $fields_map as $custobar_key => $source_key ) {
-			if ( ! in_array( $custobar_key, static::$default_keys, true ) ) {
+		foreach ($fields_map as $custobar_key => $source_key) {
+			if (!in_array($custobar_key, static::$default_keys, true)) {
 				continue;
 			}
 
-			if ( ! array_key_exists( $source_key, $data_source_fields ) ) {
+			if (!array_key_exists($source_key, $data_source_fields)) {
 				continue;
 			}
 
-			$method_or_fn = $data_source_fields[ $source_key ];
+			$method_or_fn = $data_source_fields[$source_key];
 
-			if ( ! is_callable( $method_or_fn ) && ! is_string( $method_or_fn ) ) {
+			if (!is_callable($method_or_fn) && !is_string($method_or_fn)) {
 				continue;
 			}
 
-			if ( is_string( $method_or_fn ) && method_exists( $this->data_source, $method_or_fn ) ) {
-				$method_or_fn = array( $this->data_source, $method_or_fn );
+			if (is_string($method_or_fn) && method_exists($this->data_source, $method_or_fn)) {
+				$method_or_fn = array($this->data_source, $method_or_fn);
 			}
 
-			$value = call_user_func( $method_or_fn, $this );
+			$value = call_user_func($method_or_fn, $this);
 
-			if ( is_null( $value ) ) {
+			if (is_null($value)) {
 				continue;
 			}
 
-			$properties[ $custobar_key ] = $value;
+			$properties[$custobar_key] = $value;
 		}
+
 
 		return $properties;
 	}
 
 	abstract public static function get_fields_map();
 
-	protected static function get_default_keys() {
-		$reflection = new \ReflectionClass( get_called_class() );
-		return array_values( $reflection->getConstants() );
+	protected static function get_default_keys()
+	{
+		$reflection = new \ReflectionClass(get_called_class());
+		return array_values($reflection->getConstants());
 	}
 }
