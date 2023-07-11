aa3c0f9c7d89ba28029adf69e09b08ccfcda5dd4
Samuel Gemmell <samuel@mysteerio.fi>
2023-07-11 10:23:25 +0300

    Added custom data sources feature

--- classes/data-sources/abstract-data-source.php
+++ classes/data-sources/abstract-data-source.php
@@ -2,9 +2,10 @@
 
 namespace WooCommerceCustobar\DataSource;
 
-defined( 'ABSPATH' ) || exit;
+defined('ABSPATH') || exit;
 
-abstract class Abstract_Data_Source {
+abstract class Abstract_Data_Source
+{
 
 	protected $default_keys = array();
 
@@ -12,31 +13,59 @@ abstract class Abstract_Data_Source {
 
 	public static $source_key = 'common';
 
-	public function __construct() {
+	protected static $custom_data_sources = array();
+
+
+
+	public function __construct()
+	{
 		$this->default_keys = static::get_default_keys();
 	}
 
-	protected static function get_default_keys() {
-		$reflection = new \ReflectionClass( get_called_class() );
-		return array_values( $reflection->getConstants() );
+	protected static function get_default_keys()
+	{
+		$reflection = new \ReflectionClass(get_called_class());
+		return array_values($reflection->getConstants());
 	}
 
-	public function get_fields() {
+	public function get_custom_keys()
+	{
+		return array_keys(self::$custom_data_sources);
+	}
+
+	public function get_fields()
+	{
+		$keys = $this->default_keys;
+		$custom_keys = static::get_custom_keys();
+		$custom_fields = [];
+
+
 		$fields = array_reduce(
-			$this->default_keys,
-			function( $carry, $key ) {
+			$keys,
+			function ($carry, $key) {
 				$method = "get_{$key}";
 
-				if ( ! method_exists( $this, $method ) ) {
+				if (!method_exists($this, $method)) {
 					return $carry;
 				}
 
-				$carry[ $key ] = $method;
+				$carry[$key] = $method;
 
 				return $carry;
 			},
 			array()
 		);
+
+		if ($custom_keys) {
+			$custom_fields = array_reduce(
+				$custom_keys,
+				function ($carry, $key) {
+					$carry[$key] = ["get_custom_data_source", $key];
+					return $carry;
+				},
+				array()
+			);
+		}
 
 		/**
 		 * @param array $fields
@@ -56,8 +85,28 @@ abstract class Abstract_Data_Source {
 		 *      return $fields;
 		 * }, 10, 2);
 		 */
-		$fields = apply_filters( 'woocommerce_custobar_get_' . static::$source_key . '_fields', $fields, $this );
+
+		if ($custom_fields) {
+			$fields = array_merge($fields, $custom_fields);
+		}
+		$fields = apply_filters('woocommerce_custobar_get_' . static::$source_key . '_fields', $fields, $this);
 
 		return $fields;
+	}
+
+	public static function create_custom_data_source(string $name, callable $callback)
+	{
+		self::$custom_data_sources[$name] = $callback;
+	}
+	public static function get_custom_data_source($name)
+	{
+		if (array_key_exists($name, self::$custom_data_sources)) {
+			$custom_data_source = self::$custom_data_sources[$name];
+			$callback = $custom_data_source;
+			if (is_callable($callback)) {
+				return $custom_data_source;
+			}
+		}
+		return null;
 	}
 }
--- classes/data-types/abstract-custobar-data-type.php
+++ classes/data-types/abstract-custobar-data-type.php
@@ -2,6 +2,11 @@
 
 namespace WooCommerceCustobar\DataType;
 
+use WC_Customer;
+use WooCommerceCustobar\DataSource\Abstract_Data_Source;
+use WooCommerceCustobar\DataSource\Customer;
+
+
 defined('ABSPATH') || exit;
 
 /**
@@ -17,6 +22,7 @@ abstract class Custobar_Data_Type
 
 	protected static $default_keys = array();
 
+
 	public function __construct()
 	{
 		static::$default_keys = array_keys(static::get_fields_map());
@@ -24,11 +30,28 @@ abstract class Custobar_Data_Type
 
 	public function get_assigned_properties()
 	{
+
 		$data_source_fields = $this->data_source->get_fields();
 		$fields_map = static::get_fields_map();
 		$properties = array();
 
 		foreach ($fields_map as $custobar_key => $source_key) {
+			// Custom_data_source
+			if (is_array($data_source_fields[$source_key]) && $data_source_fields[$source_key][0] === "get_custom_data_source") {
+				$method_or_fn = $data_source_fields[$source_key];
+				if ($this->data_source) {
+					$wccustomer = new WC_Customer($this->data_source->get_id());
+					$custom_data_source = Abstract_Data_Source::get_custom_data_source($method_or_fn[1]);
+					$value = call_user_func($custom_data_source, $wccustomer);
+				}
+				if (is_null($value)) {
+					continue;
+				}
+				$properties[$custobar_key] = $value;
+				continue;
+			}
+			// Custom_data_source ends
+
 			if (!in_array($custobar_key, static::$default_keys, true)) {
 				continue;
 			}
@@ -36,26 +59,22 @@ abstract class Custobar_Data_Type
 			if (!array_key_exists($source_key, $data_source_fields)) {
 				continue;
 			}
-
 			$method_or_fn = $data_source_fields[$source_key];
 
 			if (!is_callable($method_or_fn) && !is_string($method_or_fn)) {
+				error_log(wp_json_encode($method_or_fn));
 				continue;
 			}
 
 			if (is_string($method_or_fn) && method_exists($this->data_source, $method_or_fn)) {
 				$method_or_fn = array($this->data_source, $method_or_fn);
+				$value = call_user_func($method_or_fn, $this);
 			}
-
-			$value = call_user_func($method_or_fn, $this);
-
 			if (is_null($value)) {
 				continue;
 			}
-
 			$properties[$custobar_key] = $value;
 		}
-
 
 		return $properties;
 	}
